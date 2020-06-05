427fd7c783523b346c390db57c918eb3a4f445ba
Anil Yadav <iter.anilyadav@gmail.com>
2020-06-05 15:47:47 +0530

    secure api

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'secret':'nodeauthsecret',
-    'database': 'mongodb://localhost/node-auth'
-  };
+  'secret': 'nodeauthsecret',
+  'database': 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/node_auth?retryWrites=true'
+
+};
