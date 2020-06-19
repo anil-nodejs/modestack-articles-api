40256d66ef9cfe884ea2e136d60a7b575d4823df
Anil Yadav <iter.anilyadav@gmail.com>
2020-06-19 13:30:14 +0530

    added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'nodeauthsecret',
-  'database': 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/node_auth?retryWrites=true'
+  'database': 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/modestack_auth?retryWrites=true'
 
 };
